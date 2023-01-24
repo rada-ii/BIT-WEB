2376e69f33ad1a2581957757d2f96832a74b36e2
rada-i <neposebanmail@gmail.com>
2023-01-24 02:51:10 +0100

    practice

--- jQuery/practice/script.js
+++ jQuery/practice/script.js
@@ -8,35 +8,36 @@ $(document).ready(function () {
     'https://images.pexels.com/photos/5496464/pexels-photo-5496464.jpeg?auto=compress&cs=tinysrgb&w=1200',
     'https://images.pexels.com/photos/159299/graphic-design-studio-tracfone-programming-html-159299.jpeg?auto=compress&cs=tinysrgb&w=1200',
   ];
-  var title = $('<h1>').text('My Image Gallery');
-  var currentIndex = 0;
-  var img;
+  var title = $('<h1>').text('My Image Gallery').css('text-align', 'center');
+  var btn = $('#next-btn');
+  var container = $('#container');
 
+  container.css({
+    width: '1200px',
+    margin: 'auto',
+  });
   gallery.before(title);
-  showImage();
-
-  $('#next-btn').click(function () {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-    showImage();
+  btn.css('text-align', 'center');
+  btn.css({
+    display: 'block',
+    margin: 'auto',
+    'margin-top': '20px',
   });
 
-  function showImage() {
-    var randomWidth = Math.floor(Math.random() * 400) + 100;
-    var randomHeight = Math.floor(Math.random() * 400) + 100;
-    img = $('<img>').attr('src', images[currentIndex]);
+  images.forEach(function (image) {
+    img = $('<img>').attr('src', image);
     img.css({
-      width: randomWidth + 'px',
-      height: randomHeight + 'px',
+      display: 'inline-block',
+      height: '200px',
+      'margin-left': '10px',
+      'margin-right': '10px',
     });
-    if (randomWidth < 200) {
-      img.css('border', '5px solid green');
-    }
-    gallery.html(img);
+    gallery.append(img);
+  });
+  btn.on('click', function () {
     changeTitleColor();
-  }
+    changeImageSize();
+  });
 
   function changeTitleColor() {
     var letters = '0123456789ABCDEF';
@@ -45,5 +46,19 @@ $(document).ready(function () {
       color += letters[Math.floor(Math.random() * 16)];
     }
     title.css('color', color);
+  }
+  function changeImageSize() {
+    var img = $('img');
+    var randomWidth = Math.floor(Math.random() * 400) + 100;
+    var randomHeight = Math.floor(Math.random() * 400) + 100;
+    img.css({
+      width: randomWidth + 'px',
+      height: randomHeight + 'px',
+    });
+    if (randomWidth < 200) {
+      img.css('border', '5px solid green');
+    } else {
+      img.css('border', 'none');
+    }
   }
 });
